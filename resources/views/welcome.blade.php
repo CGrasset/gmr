be984947bb9d2a6718b846eb3ed59a51d080c097
cgrasset <cristian.grasset@inzpiral.com>
2019-02-23 18:49:47 -0300

    - Initial commit, setup server

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.1">
+    <title>GMR test</title>
+  </head>
+  <body style="margin-top: 150px; margin-left: 0px; background: #0e0e0e;">
+    <img style="-webkit-user-select: none;cursor: zoom-in;" src="http://giantmonkeyrobot.com/wp-content/uploads/2018/05/bg_citybuildings_logo.png">
+  </body>
+</html>
